448b68eac9621c75ba5817390cc7dee90109a0bc
sunnytechie <sunnyict001@gmail.com>
2022-12-28 00:07:13 +0100

    Updates

--- resources/views/products/independent.blade.php
+++ resources/views/products/independent.blade.php
@@ -19,7 +19,7 @@
                         <div class="product-label label-sale">-20%</div>
                     </div> --}}
                 </figure>
-                <button class="btn p-1" style="position: absolute; margin-top: -45px; margin-left: 8px; background: #0080E6; color: #fff; line-height: 16px; font-size: 10px; text-transform: capitalize; font-weight: 600">{{ $product->category->title }}</button>
+                <button class="btn p-1" style="position: absolute; margin-top: -30px; margin-left: 8px; background: #0080E6; color: #fff; line-height: 16px; font-size: 10px; text-transform: capitalize; font-weight: 600">{{ $product->category->title }}</button>
 
                 <div class="category-tag">
                     <a href="#" class="p-2 btn" style="text-transform:inherit; font-family: 'Poppins'; color: #ccc; font-size: 12px; line-height: 16px; font-weight: 400;">BuildBeta</a>
